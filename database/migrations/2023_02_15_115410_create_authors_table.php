47a2a1296b382ba00359163c176777323023dd8f
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2023-02-20 10:50:59 +0630

    migration

--- database/migrations/2023_02_15_115410_create_authors_table.php
+++ database/migrations/2023_02_15_115410_create_authors_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->longText('description');
             $table->timestamps();
         });
